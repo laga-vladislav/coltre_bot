f5a857d90607e6fc563ab7a34b1a6f082ed0fb9f
laga-vladislav <laga.ladislav@yandex.ru>
2023-09-07 22:47:47 +0900

    Изменил логику получения текста. Теперь он не находится в переменной, а формирует функцией build_text(). У каждого тэмплейта должна быть одноимённая функция.

--- coltre_bot/handlers/start_handler.py
+++ coltre_bot/handlers/start_handler.py
@@ -5,5 +5,5 @@ from coltre_bot.templates import start
 async def start_handler(bot: AsyncTeleBot, chat_id: int) -> None:
     await bot.send_message(
         chat_id=chat_id,
-        text=start.TEXT
+        text=start.build_text()
     )
