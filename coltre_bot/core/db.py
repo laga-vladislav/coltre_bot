4587c986df9f37de053fa9fb5b3eb9c7698865c4
laga-vladislav <laga.ladislav@yandex.ru>
2023-10-31 00:05:44 +0900

    Добавил fetch_one для извлечения одного поля из таблицы. Добавил insert_data для вставки новой записи в базу данных.

--- coltre_bot/core/db.py
+++ coltre_bot/core/db.py
@@ -14,8 +14,50 @@ async def fetch_all(sql_query: str) -> tuple:
     await pool.wait_closed()
     return results
 
+
+async def fetch_one(sql_query: str) -> tuple:
+    pool = await _create_pool()
+
+    async with pool.acquire() as conn:
+        async with conn.cursor() as cur:
+            await cur.execute(sql_query)
+            (r,) = await cur.fetchone()
+    pool.close()
+    await pool.wait_closed()
+    return r
+
+
+async def insert_data(sql_query: str) -> None:
+    pool = await _create_pool()
+
+    async with pool.acquire() as conn:
+        async with conn.cursor() as cur:
+            await cur.execute(sql_query)
+            await conn.commit()
+    pool.close()
+    await pool.wait_closed()
+
+
 async def _create_pool() -> aiomysql.pool:
     pool = await aiomysql.create_pool(host=config.DB_HOST, port=config.DB_PORT,
                                       user=config.DB_USERNAME, password=config.DB_PASSWORD,
                                       db=config.DB_NAME, loop=asyncio.get_running_loop())
     return pool
+
+
+async def main():
+    # from coltre_bot.core.service.user import _register_user_sql_query, User
+    # user = User(
+    #     user_id=1,
+    #     first_name='ne 1'
+    # )
+    # sql = _register_user_sql_query(user)
+    # print(sql)
+    # await insert_data(sql)
+    res = await fetch_one("select training_level_id from user where user_id=1;")
+    print(res)
+
+
+if __name__ == '__main__':
+    loop = asyncio.get_event_loop()
+    loop.run_until_complete(main())
