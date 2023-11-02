409592af252c10043c8b22c0e55cfe6aac02b751
laga-vladislav <laga.ladislav@yandex.ru>
2023-11-02 22:24:27 +0900

    Добавил MembershipRequestErrorUserDoesntExist

--- coltre_bot/exceptions.py
+++ coltre_bot/exceptions.py
@@ -9,3 +9,8 @@ class WrongMonthDigit(Exception):
 class UserAlreadyRegistered(Exception):
     """Пользователь уже есть в базе данных"""
     pass
+
+class MembershipRequestErrorUserDoesntExist(Exception):
+    """Заявка на вступление с таким айди не может быть создана, так как пользователя
+     с таким id не существует"""
+    pass
