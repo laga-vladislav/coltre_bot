3336ec65dbbd00307da9170e5b1e89932b3b79f4
laga-vladislav <laga.ladislav@yandex.ru>
2023-11-04 18:53:35 +0900

    Добавил make_user_instance, который формирует экземпляр класса User. Добавил UserValidators. Поправил небольшие ошибки, переделал логику некоторых функций, добавил комментарии. Убрал упоминание ошибки 'UserAlreadyRegistered', так как посчитал её излишней на данном этапе.

--- coltre_bot/core/service/user.py
+++ coltre_bot/core/service/user.py
@@ -4,7 +4,6 @@ import dataclasses
 from typing import Optional
 
 from coltre_bot.core.db import fetch_all, insert_data
-from coltre_bot.exceptions import UserAlreadyRegistered
 
 
 @dataclasses.dataclass
@@ -19,7 +18,7 @@ class User:
             username: Optional[str] = None,
     ):
         if not (all([user_id, first_name, training_level_id, timezone])):
-            raise ValueError("user_id, first_name, group_id cannot be None")
+            raise ValueError("user_id, first_name, training_level_id, timezone cannot be None")
         self.user_id = user_id
         self.username = username
         self.first_name = first_name
@@ -28,31 +27,65 @@ class User:
         self.timezone = timezone
 
 
-async def is_registered(user_id: int) -> bool:
-    """
-    Проверка на то, зарегистрирован ли пользователь
-    """
-    if await get_user_by_id(user_id) is not None:
-        return True
-    return False
+def make_user_instance(user_id: int,
+                       first_name: str,
+                       training_level_id: int,
+                       timezone: int,
+                       group_id: Optional[int] = None,
+                       username: Optional[str] = None) -> User:
+    return User(
+        user_id,
+        first_name,
+        training_level_id,
+        timezone,
+        group_id,
+        username
+    )
 
 
+async def get_user_by_id(user_id: int) -> User | None:
+    get_user_sql_query = _get_user_sql_query(user_id)
+    results = await fetch_all(get_user_sql_query)
+    print(results)
+    if results != ():
+        user = _parse_fetch_all(results[0])
+        return user
+    return None
+
+
+class UserValidators:
+    @staticmethod
+    def is_registered(func) -> bool:
+        """Декоратор. Выполняется, если пользователь зарегистрирован"""
+
+        async def wrapper(data: int | User):
+            if not await get_user_by_id(data if isinstance(data, int) else data.user_id):
+                return
+            await func(data)
+
+        return wrapper
+
+    @staticmethod
+    def is_not_registered(func) -> bool:
+        """Декоратор. Выполняется, если пользователь НЕ зарегистрирован"""
+
+        async def wrapper(data: int | User):
+            if await get_user_by_id(data if isinstance(data, int) else data.user_id):
+                return
+            await func(data)
+
+        return wrapper
+
+
+@UserValidators.is_not_registered
 async def register_user(user_instance: User) -> None:
-    """
-    Регистрация пользователя в базе данных
-    """
-    if not await is_registered(user_id=user_instance.user_id):
-        register_user_sql_query = _register_user_sql_query(user_instance)
-        await insert_data(register_user_sql_query)
-    else:
-        raise UserAlreadyRegistered('Пользователь уже зарегистрирован')
-
+    """Регистрация пользователя в базе данных"""
+    register_user_sql_query = _register_user_sql_query(user_instance)
+    await insert_data(register_user_sql_query)
 
 
 def _register_user_sql_query(user_instance: User):
-    """
-    Формирует SQL-скрипт на регистрацию пользователя
-    """
+    """Формирует SQL-скрипт на регистрацию пользователя"""
     params = user_instance.__dict__
     valid_keys = [key for key in params.keys() if params[key] is not None]
     keys_str = ', '.join(valid_keys)
@@ -63,21 +96,10 @@ def _register_user_sql_query(user_instance: User):
     return sql_query
 
 
-async def get_user_by_id(user_id: int) -> User | None:
-    """
-    Получить объект User, используя его айди
-    """
-    get_user_sql_query = _get_user_sql_query(user_id)
-    results = await fetch_all(get_user_sql_query)
-    if len(results) > 0:
-        user = _parse_fetch_all(results[0])
-        return user
-    return None
-
-
-async def is_group_member(user_or_user_id: int | User):
-    user = user_or_user_id
-    if isinstance(user_or_user_id, int):
+@UserValidators.is_registered
+async def is_group_member(data: int | User):
+    user = data
+    if isinstance(data, int):
         user = await get_user_by_id(user)
     if user.group_id:
         return True
@@ -85,20 +107,18 @@ async def is_group_member(user_or_user_id: int | User):
 
 
 def _get_user_sql_query(user_id: int):
-    """
-    Формирует SQL-скрипт на получение пользователя по его айди
-    """
+    """Формирует SQL-скрипт на получение пользователя по его айди"""
     return f"""SELECT * FROM user WHERE user_id = {user_id};"""
 
 
-def _parse_fetch_all(results: tuple) -> User:
+def _parse_fetch_all(result: tuple) -> User:
     return User(
-        user_id=results[0],
-        username=results[1],
-        first_name=results[2],
-        training_level_id=results[3],
-        group_id=results[4],
-        timezone=results[5]
+        user_id=result[0],
+        username=result[1],
+        first_name=result[2],
+        training_level_id=result[3],
+        group_id=result[4],
+        timezone=result[5]
     )
 
 
@@ -109,8 +129,7 @@ async def main():
         training_level_id=2,
         timezone=4
     )
-    ff = await is_group_member(user)
-    print(ff)
+    print(make_user_instance(1, '2', 3, 4).__dict__)
 
 
 if __name__ == '__main__':
